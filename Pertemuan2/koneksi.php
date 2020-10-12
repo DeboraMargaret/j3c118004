84341488e7bf85d7a965f8eaf10f86a294fb6bbf
j3c118004 <72422604+j3c118004@users.noreply.github.com>
2020-10-12 20:33:25 +0700

    Add files via upload
    Pertemuan 2

---
+++ Pertemuan2/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+    $host   = "sqlXXX.epizy.com";//"localhost";
+    $user   = "epiz_26781493";//"root";
+    $psw    = "0n5BoJFBSnlh";//"";
+    $db_name= "epiz_26781493_J3C118004";//"pabw";
+    $kon = mysqli_connect($host,$user,$psw,$db_name);
+
+?>
